66c7df924d10b1f028d6f593a451c6bbf6a9c5f9
illonion <zhou.lawrence.yz@gmail.com>
2024-05-02 01:07:19 +1000

    Add tasks, filter by date and user

--- app/src/main/java/com/example/todolist/Home.kt
+++ app/src/main/java/com/example/todolist/Home.kt
@@ -2,8 +2,6 @@ package com.example.todolist
 
 import android.annotation.SuppressLint
 import android.os.Build
-import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -14,21 +12,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.Card
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -48,11 +38,11 @@ import androidx.navigation.NavHostController
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 // Home screen
 @OptIn(ExperimentalMaterial3Api::class)
@@ -160,13 +150,23 @@ fun Home(navController: NavHostController, viewModel: ToDoListItemViewModel) {
             )
 
             var toDoListItems by remember { mutableStateOf(emptyList<ToDoListItem>()) }
+            val currentUserUid = Firebase.auth.currentUser?.uid
             viewModel.allToDoListItems.observeAsState(emptyList()).apply {
-                toDoListItems = this.value.sortedBy { it.createdAt }
+                toDoListItems = this.value
+                    .filter { it.userId == currentUserUid || it.friend == currentUserUid } // Remove items where the user is not the user OR a friend
+                    .filter { LocalDate.parse(it.dueDate, DateTimeFormatter.ofPattern("dd/MM/yyyy")) == LocalDate.now() } // Filter by today
+                    .filter { !it.completed} // Filter by completed or not completed
+                    .sortedBy { it.createdAt } // Sort by creation date
             }
-            Column() {
-                LazyColumn {
-                    itemsIndexed(toDoListItems) { index, item ->
-                        ListToDoListItem(item, false)
+
+            if (toDoListItems.isEmpty()) {
+                Text(text = "There are currently no items in today's task list!")
+            } else {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    LazyColumn {
+                        itemsIndexed(toDoListItems) { index, item ->
+                            ListToDoListItem(item, false)
+                        }
                     }
                 }
             }
