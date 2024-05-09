4837355d92f934d77832d538f2da126d5eae876d
lpq66 <lpq66632@gmail.com>
2024-05-09 16:37:25 +1000

    weather_folder and miscellaneous files comment updated

--- app/src/main/java/com/example/todolist/DatabaseActivity.kt
+++ app/src/main/java/com/example/todolist/DatabaseActivity.kt
@@ -11,23 +11,29 @@ import com.example.todolist.LoginSignup.AuthenticationActivity
 
 class DatabaseActivity {
 
+    // Initializes a Firebase Realtime Database instance
     private val database =
-        FirebaseDatabase.getInstance("http://fit5046-assignment-3-5083c-default-rtdb.asia-southeast1.firebasedatabase.app")
+        FirebaseDatabase.getInstance("http://fit5046-assignment-3-5083c-default-rtdb." +
+                "asia-southeast1.firebasedatabase.app")
 
+    // Make sure no multiple devices log in same account at the same time
     fun checkValidSession(context: Context, isValidSession: (Boolean) -> Unit) {
         getCurrentSessionTokenCallback { dbSessionToken ->
             AuthenticationActivity().getTokenCallback {authToken ->
                 if (dbSessionToken == authToken && authToken != null) {
                     isValidSession(true)
                 } else {
-                    Toast.makeText(context, "New login detected on another device. Please login again.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, "New login detected on another device. " +
+                            "Please login again.", Toast.LENGTH_LONG).show()
                     isValidSession(false)
                 }
             }
         }
     }
 
-    fun addNewUser(user: FirebaseUser?, email: String, firstName: String, lastName: String, isSuccess: (Boolean) -> Unit) {
+    // Registration
+    fun addNewUser(user: FirebaseUser?, email: String, firstName: String,
+                   lastName: String, isSuccess: (Boolean) -> Unit) {
         user?.let {
             val userMap = hashMapOf(
                 "firstName" to firstName,
@@ -44,8 +50,10 @@ class DatabaseActivity {
                 }
         } ?: run {isSuccess(false)}
     }
+
+    // Retrieve the session token of the current user
     private fun getCurrentSessionTokenCallback(sessionToken: (String?) -> Unit) {
-//        This is the one that can be used (using callback)
+        // This is the one that can be used (using callback)
         val user = AuthenticationActivity().getUser()
         user?.let {
             val ref = database.getReference("users").child(user.uid).child("sessionToken")
@@ -70,6 +78,7 @@ class DatabaseActivity {
         }
     }
 
+    // Set the session token for a given Firebase user
     fun setCurrentSessionId(user: FirebaseUser?, sessionToken: String?) {
         user?.let {
             database.reference.child("users").child(user.uid).child("sessionToken").setValue(sessionToken)
